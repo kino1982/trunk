e1c11b1dceeff661709df2943b187a292f4a8268
kino <kin5.ki6.xxx@gmail.com>
2021-05-07 15:11:05 +0900

    htmlのtable部分を記述する関数を追加

--- table/js/org/table.js
+++ table/js/org/table.js
@@ -1,4 +1,36 @@
-function test(){
-	$("#kino").text("kinokino");
+function write_table(table_ary){
+	//*************************
+	// table部分を記述する関数
+	//*************************
+
+	var tr_s = "";
+
+	// table内のtrを記述する
+	// table_aryは２次元配列
+
+	for (var ii=0;ii<table_ary.length;ii++){
+		tr_s = tr_s + write_tr(table_ary[ii]);
+	}
+
+	//tableを記述	
+	var table_s = "<table border='1'>" + tr_s + "</table>"; 
+	$("#kino").append(table_s);
+
 	console.log("testだよ");
+}
+
+function write_tr(td_ary){
+	//*************************
+	// tableのtrを記述する関数
+	//*************************
+	
+	var s = "";
+
+	for (var ii=0;ii<td_ary.length;ii++){
+		s = s + "<td>" + td_ary[ii] + "</td>";
+	}
+
+	s = "<tr>" + s + "</tr>";
+
+	return s;
 }
