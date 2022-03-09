193f8eadb298ed31eba266fb3553fc0c0401dcf7
kino1982 <kin5.ki6.xxx@gmail.com>
2022-03-09 14:15:32 +0900

    修正

--- javascript/react/chohyo/App.jsx
+++ javascript/react/chohyo/App.jsx
@@ -2,53 +2,71 @@ import {useState} from "react";
 import { useContext } from "react";
 import {ColoredMessage} from "./components/ColoredMessage.jsx";
 import {MkTbody} from "./components/MkTbody.jsx";
+import {MkTr} from "./components/MkTr.jsx";
 import {MkSelectBox} from "./components/MkSelectBox.jsx";
 import {MkP} from "./components/MkP.jsx";
 import { AreaNumContext } from "./components/AreaNumProvider";
+import { ElemContext } from "./components/ElemProvider";
+import { FlvContext } from "./components/FlvProvider";
+import { InitTimeContext } from "./components/InitTimeProvider";
+import { MkThead } from "./components/MkThead.jsx";
 
 
 export const App = () => {
-
-	const [num, setNum] = useState(0);
-
-	const onClickButton = () => {
-		setNum(num + 1);
-	};
 	
 	let kino = [100, 101, 102, 104];
 	
 	const {big_area_num} = useContext(AreaNumContext);
+	const {elem} = useContext(ElemContext);
+	const {flv} = useContext(FlvContext);
+	const {init_time} = useContext(InitTimeContext);
 	
 	let kino_data = {
 						"0000":{
-								"01":[[100,100], [101,101],[102,102],[103,103]],
-								"02":[[200,200],[201,201],[202,202]]
+								"01":[
+										[100,100,101,101,101,101,101,101,101,101,101,101,101,102], 
+										[101,101,101,101,101,101,101,101,101,101,101,101,101,102],
+										[102,102,101,101,101,101,101,101,101,101,101,101,101,102],
+										[103,103,101,101,101,101,101,101,101,101,101,101,101,102]
+									],
+								"02":[
+										[200,200,101,101,101,101,101,101,101,101,101,101,101,102],
+										[201,201,101,101,101,101,101,101,101,101,101,101,101,102],
+										[202,202,101,101,101,101,101,101,101,101,101,101,101,102]
+									]
 						},
 						"0001":{
-								"03":[[100,100], [101,101],[102,102],[103,103]],
-								"04":[[200,200],[201,201],[202,202]]
+								"03":[
+										[100,100,101,101,101,101,101,101,101,101,101,101,101,102], 
+										[101,101,101,101,101,101,101,101,101,101,101,101,101,102],
+										[102,102,101,101,101,101,101,101,101,101,101,101,101,102],
+										[103,103,101,101,101,101,101,101,101,101,101,101,101,102]
+									],
+								"04":[
+										[200,200,101,101,101,101,101,101,101,101,101,101,101,102],
+										[201,201,101,101,101,101,101,101,101,101,101,101,101,102],
+										[202,202,101,101,101,101,101,101,101,101,101,101,101,102]
+									]
 						}
 	}
 	
 	let tbody_datas = kino_data[big_area_num];
 	
-	console.log(tbody_datas);
+	let file_name = `${init_time}_${elem}_${flv}.json`;
 
 	return (
 	
 		<>
 			<h1 style = {{color : "red"}}>hello world</h1>
-			<ColoredMessage color="blue" >お元気ですか？</ColoredMessage>
-			<button onClick={onClickButton}>ボタン</button>
-			<ColoredMessage color="pink" >元気です</ColoredMessage>
-			<p>{num}</p>
 			<MkSelectBox></MkSelectBox>
 			<MkP></MkP>
+			<p>{file_name}</p>
+			
 			<table>
-				<thead>
-					<MkTr tr_ary={kino}></MkTr>
-				</thead>
+				
+				<MkThead init_time={init_time} model="msm" elem="R3"></MkThead>
 				<MkTbody big_area_num={big_area_num} tbody_datas={tbody_datas}></MkTbody>
 			</table>
+		</>
 	);
 };
--- javascript/react/chohyo/index.js
+++ javascript/react/chohyo/index.js
@@ -1,10 +1,19 @@
 import ReactDOM from "react-dom";
 import { App } from "./App.jsx";
 import { AreaNumProvider } from "./components/AreaNumProvider.jsx";
+import { ElemProvider } from "./components/ElemProvider.jsx";
+import { FlvProvider } from "./components/FlvProvider.jsx";
+import { InitTimeProvider } from "./components/InitTimeProvider.jsx";
 
 ReactDOM.render(
 	<AreaNumProvider>
-		<App />
+		<ElemProvider>
+			<FlvProvider>
+				<InitTimeProvider>
+					<App />
+				</InitTimeProvider>
+			</FlvProvider>
+		</ElemProvider>
 	</AreaNumProvider>, 
 	document.getElementById("root"));
 
